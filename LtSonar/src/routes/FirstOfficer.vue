d63fcbc8b8efcfe34ee5e578d485703f8eccd455
Deryana <deredhuzent@gmail.com>
2020-06-01 23:17:59 -0700

    first officer fixed view

--- LtSonar/src/routes/FirstOfficer.vue
+++ LtSonar/src/routes/FirstOfficer.vue
@@ -3,29 +3,66 @@ VFS VUE Single File Component
 Copyright (c) 2018. Scott Henshaw, Kibble Online Inc. All Rights Reserved.
 -->
 <template>
-
+    <!-- Main Container -->
     <section class="firstOfficer-container">
         <div class="firstOfficer">
+            <!-- Title -->
             <h1 class="title">{{ name }}</h1>
         </div>
 
-        <div class="contentContainer">
-
-
-        <div class="mapContainer">
-            <p>Map goes here</p>
-        </div>
-
-        <div class="toolsMenu">
-            <div class="tools">
-            <p>Tools Map</p>
+<!--  CHAT / DAMAGE / CHARGE -->
+            <div class="topContainer">
+                <div class="damage">
+                    <p>Damage</p>
+                </div>
+                <div class="charge">
+                    <p>charge</p>
+                </div>
+                <!-- ChatBox -->
+                <div class="chat">
+                    <p>Chat</p>
+                </div>
             </div>
 
-            <div class="chat">
-                <p>chat</p>
-            </div>
-        </div>
 
+<!-- ACTIONS  -->
+        <div class="actions">
+         <!-- BOX 1 -->
+            <div class="attack">
+                <div class="box">
+                    <!-- mines -->
+                    <p>mines</p>
+                </div>
+                <div class="box">
+                    <!-- torpedo -->
+                    <p>torpedo</p>
+                </div>
+            </div>
+
+        <!-- BOX 2 -->
+            <div class="recon">
+                <div class="box">
+                    <!-- drones -->
+                    <p>drones</p>
+                </div>
+                <div class="box">
+                    <!-- sonar -->
+                    <p>sonar</p>
+                </div>
+            </div>
+
+        <!-- BOX 3 -->
+            <div class="deffense">
+                <div class="box">
+                    <!-- silent -->
+                    <p>silent</p>
+                </div>
+                <div class="box">
+                    <!-- scenario -->
+                    <p>scenario</p>
+                </div>
+            </div>
+    
         </div>
 
     </section>
@@ -34,9 +71,10 @@ Copyright (c) 2018. Scott Henshaw, Kibble Online Inc. All Rights Reserved.
 
 <script>
     import Controller from '@/../lib/controller'
+    import lsChat from '@/components/Chat.vue'
 
     class FirstOfficerController extends Controller {
-
+        
         constructor( name, subComponentList = []) {
             super( name, subComponentList );
             this.vm = {
@@ -52,76 +90,66 @@ Copyright (c) 2018. Scott Henshaw, Kibble Online Inc. All Rights Reserved.
 <style scoped>
 /* Local styles for First officer View */
     .firstOfficer-container {
+        color: black;
+        text-align: center;
+
         display: inline-block;
-        width: 100vw;
-        background-color: lightgray;
+        width: 90vw;
+
+        display: grid;
+        grid-template-rows: .5fr 1fr 2fr 2fr;
+        position: relative;
+
+        top: 0;
+        bottom: 0;
     }
 
-    .title {
-        margin-top: 3%;
-        margin-left: 4%;
-        position: absolute;
-        color: black;
+    .title{
+        margin-top: 2%;
     }
 
     p{
         color: black;
-        margin: 1%;
     }
 
-    .contentContainer {
-        margin: 2vw;
-        padding: 2%;
-        border: 2px solid black;
-        height: 70vh;
-        width: 84vw;
+/* TOP CONTAINER */
+    .topContainer {
+        border: 1px solid black;
+        margin-left: 1%;
+        margin-right: 3%;
+        margin-bottom: 3%;
+        display: grid;
+        grid-template-columns: 2fr 2fr 2fr;
+
+    }
+
+/* ACTIONS CONTAINER */
+    .actions {
+        border: 1px solid black;
+        margin-right: 3%;
+        margin-left: 1%;
 
         display: flex;
         flex-direction: row;
-        align-content: center;
-        justify-content: space-between;
+        justify-content: center;
     }
 
-    .mapContainer {
-        border: 2px solid black;
-        width: 40vw;
-        height: 55vh;
-        margin-top: 2%;
-
-        text-align: center;
-    }
-
-    .toolsMenu {
-        margin-top: 2%;
-        padding: 1%;
-        border: 2px solid black;
-        width: 38vw;
-        height: 55vh;
+    .box{
+        border: 1px solid black;
+        width: 510px;
+        height: 210px;
 
         display: flex;
-        flex-direction: column;
         align-items: center;
-
-        text-align: center;
+        flex-direction: column;
     }
 
-    .tools {
-        margin-top: 2%;
-        border: 2px solid black;
-        width: 30vw;
-        height: 30vh;
-
-        text-align: center;
-    }
-
-    .chat {
-        margin: 2%;
-        border: 2px solid black;
-        width: 30vw;
-        height: 20vh;
-
-
-        text-align: center;
-    }
+    /* .mines {
+        background-image: url("/LtSonar/src/assets/mine.png");
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-color: chocolate;
+    } */
 
 </style>
