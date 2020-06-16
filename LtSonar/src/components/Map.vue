6e9f97b35e9be224dd1af25bdc6d9ccabfde24a5
Alex <pg18alejandro@vfs.com>
2020-06-15 20:11:56 -0700

    map with ids

--- LtSonar/src/components/Map.vue
+++ LtSonar/src/components/Map.vue
@@ -3,8 +3,8 @@
 -->
 <template>
 
-    <section class="component-style">  <!-- Just one main element per template -->
-        <table>
+    <section class="component-style">  
+        <div>
             <div class="row" >
                 <h3 v-for="(letter, i) in axisX" :key="i" class="axis-x">{{letter}}</h3>
             </div>
@@ -13,20 +13,18 @@
                     <h3 v-for="n in axisY[m - 1]" :key="n" class="axis-y">{{ n }}</h3>
                 </div>
                 <div v-for="n in 3" :key="n" class="row sector">
-                    <div v-for="n in 5" :key="n">
-                        <div v-for="n in 5" :key="n" class="tile waterTile"></div>
+                    <div v-for="k in 5" :key="k">
+                        <div v-for="n in 5" :key="n" class="tile waterTile" :id="axisX[k + (5 * m)]+axisY[m - 1][n-1]"></div>
                     </div>
                 </div>
             </div>
-        </table>
+        </div>
     </section>
 
 </template>
 
 <script>
     import Controller from '@/../lib/controller'
-
-    // import other components you use here...
 
     class MapController extends Controller {
 
@@ -36,11 +34,9 @@
                 axisX: ["", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O"],
                 axisY: [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]],
             }
-            this.props = { // props are passed in when using this component
+            this.props = {
             }
         }
-
-
     }
 
     export default new MapController('lsMap');
@@ -48,10 +44,6 @@
 </script>
 
 <style scoped>
-    /*
-    Add "scoped" attribute to limit CSS to this component only <style scoped>
-    styles that are specific to this component only, not sub-children
-    */
 
     .tile {
         border: 1px solid black;
